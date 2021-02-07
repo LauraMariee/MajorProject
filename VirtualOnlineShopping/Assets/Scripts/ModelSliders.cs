b7b61fed6b4fa4cd6ceb3edde2c138ee1e6be27d
Laura <lauramarie8131@gmail.com>
2021-02-07 18:14:38 +0100

    Created prefab and started script

---
+++ VirtualOnlineShopping/Assets/Scripts/ModelSliders.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSliders : MonoBehaviour
+{
+    private int defaultBustSize;
+    private int defaultWaistSize;
+    private int defaultHipSize;
+
+    public int minBustSize;
+    public int maxBustSize;
+
+    public int minWaistSize;
+    public int maxWaistSize;
+
+    public int minHipSize;
+    public int maxHipSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultBustSize = 35;
+        defaultWaistSize = 28;
+        defaultHipSize = 37;  
+}
+
+// Update is called once per frame
+void Update()
+    {
+        //check position 
+        //Get value from the range
+    }
+
+    public void checkPosition()
+    {
+
+    }
+}
