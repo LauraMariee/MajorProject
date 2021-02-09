029a1f92767bae9811d3e80109cc6367a39308b6
Laura <lauramarie8131@gmail.com>
2021-02-09 21:19:43 +0100

    Created algorithm and started look up table

---
+++ VirtualOnlineShopping/Assets/Scripts/LookUpTables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookUpTables : MonoBehaviour
+{
+    public Dictionary<int, int> InchesDefault = new Dictionary<>() { get; }
+    public HashSet<int> ReservedKeys { get; }
+
+    public void LookupTable()
+    {
+        inchesDefault.Add(0, 31, 24, 33);
+        inchesDefault.Add(1, 32, 25, 34);
+        inchesDefault.Add(2, 33, 26, 35);
+        inchesDefault.Add(3, 35, 28, 37);
+        inchesDefault.Add(4, 37, 30, 39);
+        inchesDefault.Add(5, 39, 31, 41);
+        inchesDefault.Add(6, 41, 33, 43);
+        inchesDefault.Add(7, 44, 36, 46);
+
+        ReservedKeys = new HashSet<int>();
+    }
+
+    public string Lookup(int key)
+    {
+        return (ReservedKeys.Contains(key))
+           ? null
+           : inchesDefault[key];
+    }
+}
--- VirtualOnlineShopping/Assets/Scripts/ModelSliders.cs
+++ VirtualOnlineShopping/Assets/Scripts/ModelSliders.cs
@@ -4,84 +4,27 @@ using UnityEngine;
 
 public class ModelSliders : MonoBehaviour
 {
-    public int defaultValue;
-    public int currentValue;
-
     public int minSize;
     public int maxSize;
 
+    public GameObject startPoint;
+    public GameObject endPoint;
+
     public GameObject physicalHandle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentValue = defaultValue; 
-    }
-
-// Update is called once per frame
-    void FixedUpdate()
-    {
-        //Get value from the range
-    }
 
     public float getXPosition()
     {
         float handlePosition = physicalHandle.GetComponent<Transform>().position.x;
         return handlePosition;
-        
     }
 
-
-    private bool checkBounds()
+    public int calculateHandleDifference()
     {
-        Debug.Log("checkBounds"); 
-        if (currentValue == maxSize)
-        {
-            return false;
-        }
-        else if(currentValue == minSize)
-        {
-            return false; 
-        }
-        return true; 
-    }
+        var physicalDifference = startPoint - endPoint;
+        var valueDifference = maxSize - minSize;
 
-    private void updateValue()
-    {
-        Debug.Log("UpdateValue");
-        if (getXPosition() > defaultValue)
-        {
-            Debug.Log("Bigger Value");
-            if (checkBounds())
-            {
-                currentValue++;
-                Debug.Log("Value goes up");
-            }
-            
-        }
-        else if(getXPosition() < defaultValue)
-        {
-            Debug.Log("Smaller Value");
-            if (checkBounds())
-            {
-                currentValue--;
-                Debug.Log("Value goes down");
-            }
-            
-        }
-        else
-        {
-            Debug.Log("No Change"); 
-        }
-        
-        //x position increase value
-        //Increment by 1
-    }
-
-
-
-    public void checkHandleValue()
-    {
-
+        var increment = physicalDifference / valueDifference;
+        return increment; 
     }
 }
