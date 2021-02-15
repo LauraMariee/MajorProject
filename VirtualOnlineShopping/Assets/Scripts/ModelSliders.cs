13a20a40ecca87a3a88b6358a18515afee009e12
Laura <lauramarie8131@gmail.com>
2021-02-15 15:08:32 +0100

    Slider work with all values and axis

--- VirtualOnlineShopping/Assets/Scripts/ModelSliders.cs
+++ VirtualOnlineShopping/Assets/Scripts/ModelSliders.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -14,76 +15,40 @@ public class ModelSliders : MonoBehaviour
 
     public GameObject physicalHandle;
     
-    private Dictionary<float, int> handleValueList = new Dictionary<float, int>();
+    private int PositionToValue(Vector3 position)
+    {
+        var startPosition = startPoint.GetComponent<Transform>().position;
+        var endPosition = endPoint.GetComponent<Transform>().position;
+        var fraction = (position - startPosition).magnitude / (endPosition - startPosition).magnitude;
 
+        var value = minSize + fraction * (maxSize - minSize);
+        return (int)Math.Round(value);
+    }
+
+    private Vector3 ValueToPosition(float value)
+    {
+        var fraction = (value - minSize) / (maxSize - minSize);
+
+        var startPosition = startPoint.GetComponent<Transform>().position;
+        var endPosition = endPoint.GetComponent<Transform>().position;
+        var position = startPosition + (endPosition - startPosition) * fraction;
+        return position;
+    }
+    
     public void Start()
     {
         startPoint = GameObject.Find("Start");
         endPoint = GameObject.Find("End");
-        
-        AssignData();
     }
 
-    private float GetHandlePosition()
+    private Vector3 GetHandlePosition()
     {
-        var handlePosition = physicalHandle.GetComponent<Transform>().position.x;
+        var handlePosition = physicalHandle.GetComponent<Transform>().position;
         return handlePosition;
-    }
-
-    private float CalculateHandleDifference()
-    {
-        var physicalDifference = startPoint.GetComponent<Transform>().position.x - endPoint.GetComponent<Transform>().position.x;
-        float valueDifference = maxSize - minSize;
-
-        var increment = physicalDifference / valueDifference;
-        return increment; 
-    }
-
-    private void AssignData()
-    {
-        var iteration = CalculateHandleDifference();
-        var currentValue = minSize;
-        var newHandleValue = startPoint.GetComponent<Transform>().position.x;
-
-        for (var i = 0; i <= (maxSize - minSize); i++)
-        {
-            var handleValue = newHandleValue + iteration;
-            SaveData(handleValue, currentValue);
-            //Debug.Log(currentValue);
-
-            currentValue++;
-            newHandleValue = handleValue; 
-        }
-    }
-
-    private int GetDataForHandlePos()
-    {
-        //get handle position
-        //Search through List until values match
-        if (!handleValueList.Any()) return 0;
-        var value = handleValueList[GetHandlePosition()];//get value
-        return value;
-
-    }
-
-    private void SaveData(float pos, int value)
-    {
-        handleValueList.Add(pos, value);
-    }
-
-    private void DisplayData()
-    {
-        foreach (var key in handleValueList)
-        {
-            Debug.Log("Handle pos is " + GetHandlePosition());
-            Debug.Log("Value is " + GetDataForHandlePos());
-            Debug.Log("Dictionary data is " + key);
-        }
     }
 
     public void Update()
     {
-        DisplayData();
-       
+        Debug.Log(PositionToValue(GetHandlePosition()));
     }
 }
