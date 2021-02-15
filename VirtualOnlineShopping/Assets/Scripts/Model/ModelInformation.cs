c9fa8587ca63951ae5dc533c3746a26ae4023ad6
Laura <lauramarie8131@gmail.com>
2021-02-15 18:14:10 +0100

    model values linked to levers

--- VirtualOnlineShopping/Assets/Scripts/Model/ModelInformation.cs
+++ VirtualOnlineShopping/Assets/Scripts/Model/ModelInformation.cs
@@ -1,25 +1,64 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Model
 {
     public class ModelInformation : MonoBehaviour
-    {
+    { 
+        public int bust;
         public int hips;
         public int waist;
-        public int bust;
         public int shoulder;
         public int neck;
 
-        // Start is called before the first frame update
-        void Start()
+        private const string Root = "stand/";
+
+        public List<string> bodyObjectList = new List<string>(){ "BustHandle", "HipHandle", "WaistHandle", "ShoulderHandle", "NeckHandle"}; //Keep this order
+        public List<string> integerNameList = new List<string>() {"bust", "hips", "waist", "shoulder", "neck"}; //keep this order
+
+        private void CreateGameObjects()
         {
+            foreach (var key in bodyObjectList)
+            {
+                var keyObject = GameObject.Find(CreateString(Root, key));//create and find new gameObject
+                foreach (var integerName in integerNameList)
+                {
+                    switch (integerName)
+                    {
+                        case "bust":
+                            bust = keyObject.GetComponent<ModelSliders>().AssignText();
+                            break;
+                        case "hips":
+                            hips = keyObject.GetComponent<ModelSliders>().AssignText();
+                            break;
+                        case "waist":
+                            waist = keyObject.GetComponent<ModelSliders>().AssignText();
+                            break;
+                        case "shoulder":
+                            shoulder = keyObject.GetComponent<ModelSliders>().AssignText();
+                            break;
+                        case "neck":
+                            neck = keyObject.GetComponent<ModelSliders>().AssignText();
+                            break;
+                    }
+                }
+            }
+        }
         
+        private static string CreateString(string rootString, string key)
+        {
+            var newString = rootString + key;
+            return newString;
         }
 
+
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            CreateGameObjects();
         }
+        
+        
     }
 }
--- VirtualOnlineShopping/Assets/Scripts/Model/ModelSliders.cs
+++ VirtualOnlineShopping/Assets/Scripts/Model/ModelSliders.cs
@@ -1,63 +1,63 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ModelSliders : MonoBehaviour
+namespace Model
 {
-    public int minSize;
-    public int maxSize;
+    public class ModelSliders : MonoBehaviour
+    {
+        public int minSize;
+        public int maxSize;
 
-    private GameObject startPoint;
-    private GameObject endPoint;
+        private GameObject startPoint;
+        private GameObject endPoint;
 
-    public GameObject physicalHandle;
+        public GameObject physicalHandle;
 
-    public Text handleText;
+        public Text handleText;
+
+        private int PositionToValue(Vector3 position)
+        {
+            var startPosition = startPoint.GetComponent<Transform>().position;
+            var endPosition = endPoint.GetComponent<Transform>().position;
+            var fraction = (position - startPosition).magnitude / (endPosition - startPosition).magnitude;
+
+            var value = minSize + fraction * (maxSize - minSize);
+            return (int)Math.Round(value);
+        }
+
+        private Vector3 ValueToPosition(float value)
+        {
+            var fraction = (value - minSize) / (maxSize - minSize);
+
+            var startPosition = startPoint.GetComponent<Transform>().position;
+            var endPosition = endPoint.GetComponent<Transform>().position;
+            var position = startPosition + (endPosition - startPosition) * fraction;
+            return position;
+        }
     
-    public int PositionToValue(Vector3 position)
-    {
-        var startPosition = startPoint.GetComponent<Transform>().position;
-        var endPosition = endPoint.GetComponent<Transform>().position;
-        var fraction = (position - startPosition).magnitude / (endPosition - startPosition).magnitude;
+        private void Start()
+        {
+            startPoint = GameObject.Find("Start");
+            endPoint = GameObject.Find("End");
+        }
 
-        var value = minSize + fraction * (maxSize - minSize);
-        return (int)Math.Round(value);
+        private Vector3 GetHandlePosition()
+        {
+            var handlePosition = physicalHandle.GetComponent<Transform>().position;
+            return handlePosition;
+        }
+
+        private void Update()
+        {
+            AssignText();
+        }
+
+        public int AssignText()
+        {
+            handleText.text = PositionToValue(GetHandlePosition()).ToString();
+            return Convert.ToInt32(handleText.text);
+        }
+
     }
-
-    private Vector3 ValueToPosition(float value)
-    {
-        var fraction = (value - minSize) / (maxSize - minSize);
-
-        var startPosition = startPoint.GetComponent<Transform>().position;
-        var endPosition = endPoint.GetComponent<Transform>().position;
-        var position = startPosition + (endPosition - startPosition) * fraction;
-        return position;
-    }
-    
-    private void Start()
-    {
-        startPoint = GameObject.Find("Start");
-        endPoint = GameObject.Find("End");
-    }
-
-    private Vector3 GetHandlePosition()
-    {
-        var handlePosition = physicalHandle.GetComponent<Transform>().position;
-        return handlePosition;
-    }
-
-    private void Update()
-    {
-        AssignText();
-    }
-
-    private void AssignText()
-    {
-        handleText.text = PositionToValue(GetHandlePosition()).ToString();
-    }
-
 }
