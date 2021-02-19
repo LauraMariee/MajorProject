dbcb97751e3feb5f086fd46d7aa1ebe0553860d3
Laura <lauramarie8131@gmail.com>
2021-02-19 16:05:01 +0100

    All bones linked up and can move

--- VirtualOnlineShopping/Assets/Scripts/Model/ModelInformation.cs
+++ VirtualOnlineShopping/Assets/Scripts/Model/ModelInformation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Valve.VR;
@@ -8,71 +9,106 @@ namespace Model
     public class ModelInformation : MonoBehaviour
     {
         private int bust;
-        public int hips;
-        public int waist;
-        public int shoulder;
-        public int neck;
+        private int hips;
+        private int waist;
+        private int shoulder;
+        private int neck;
 
-        public List<GameObject> chestObject = new List<GameObject>(); //get game objects for the chest
+        private const float MINScale = 0.8f;
+        private const float MAXScale = 1.8f;
 
-        private GameObject bustObject;
-        private GameObject hipObject; 
-        private GameObject waistObject; 
-        private GameObject shoulderObject; 
-        private GameObject neckObject; 
+        public List<GameObject> bustBones = new List<GameObject>(); //get game objects for the chest
+        public List<GameObject> hipBones = new List<GameObject>(); //get game objects for the chest
+        public List<GameObject> waistBones = new List<GameObject>(); //get game objects for the chest
+        public List<GameObject> shoulderBones = new List<GameObject>();
+        public List<GameObject> neckBones = new List<GameObject>();
+        
+        private ModelSliders bustObject;
+        private ModelSliders hipObject; 
+        private ModelSliders waistObject; 
+        private ModelSliders shoulderObject; 
+        private ModelSliders neckObject;
+
+        private readonly List<string> modelVariables = new List<string>() { "bust", "hips", "waist", "shoulder", "neck"};
         
         public void Start()
         {
             CreateGameObjects();
-            SetHandleValue(0.8f, 1.3f, bustObject.GetComponent<ModelSliders>(), bust);
-            SetHandleValue(0.8f, 1.3f, hipObject.GetComponent<ModelSliders>(), hips); 
-            SetHandleValue(0.8f, 1.3f, waistObject.GetComponent<ModelSliders>(), waist); 
-            SetHandleValue(0.8f, 1.3f, shoulderObject.GetComponent<ModelSliders>(), shoulder); 
-            SetHandleValue(0.8f, 1.3f, neckObject.GetComponent<ModelSliders>(), neck); 
+            SetHandleValue(MINScale, MAXScale, bustObject, bust);
+            SetHandleValue(MINScale, MAXScale, hipObject, hips); 
+            SetHandleValue(MINScale, MAXScale, waistObject, waist); 
+            SetHandleValue(MINScale, MAXScale, shoulderObject, shoulder); 
+            SetHandleValue(MINScale, MAXScale, neckObject, neck); 
         }
 
         private void CreateGameObjects()
         {
-            bustObject = GameObject.Find("BustHandle");
-            hipObject = GameObject.Find("HipHandle");
-            waistObject = GameObject.Find("WaistHandle");
-            shoulderObject = GameObject.Find("ShoulderHandle");
-            neckObject = GameObject.Find("NeckHandle");
+            bustObject = GameObject.Find("BustHandle").GetComponent<ModelSliders>();
+            hipObject = GameObject.Find("HipHandle").GetComponent<ModelSliders>();
+            waistObject = GameObject.Find("WaistHandle").GetComponent<ModelSliders>();
+            shoulderObject = GameObject.Find("ShoulderHandle").GetComponent<ModelSliders>();
+            neckObject = GameObject.Find("NeckHandle").GetComponent<ModelSliders>();
         }
         
-
-        // Update is called once per frame
         private void Update()
         {
-            UpdateHandleValue(bustObject.GetComponent<ModelSliders>());
+            UpdateHandleValue(bustObject, hipObject, waistObject, shoulderObject, neckObject);
         }
-
 
         private static float SetHandleValue(float minScale, float maxScale, ModelSliders handle, int currentValue)
         {
-            float minSize = handle.minSize;//set range of values - min and max
-            float maxSize = handle.maxSize;//set range of values - min and max
+            //set range of values - min and max
+            float minSize = handle.minSize;
+            float maxSize = handle.maxSize;
+            
             var fraction = (currentValue - minSize) / (maxSize - minSize);
             var value = minScale + fraction * (maxScale - minScale); //change scale
+            Debug.Log("Value is " + value);
             return value;
         }
 
-        private void UpdateHandleValue(ModelSliders handle)
+        private void UpdateHandleValue(ModelSliders bustHandle, ModelSliders hipHandle, ModelSliders waistHandle, ModelSliders shoulderHandle, ModelSliders neckHandle)
         {
-            bust = handle.AssignText();
-            ScaleChestByValue(SetHandleValue(0.8f, 1.3f, handle.GetComponent<ModelSliders>(), bust));
+            foreach (var key in modelVariables)
+            {
+                var caseSwitch = key;
+
+                switch (caseSwitch)
+                {
+                    case "bust":
+                        bust = bustHandle.AssignText();
+                        ScaleByValue(SetHandleValue(MINScale, MAXScale, bustHandle, bust), bustBones);
+                        break;
+                    case "hips":
+                        hips = hipHandle.AssignText();
+                        ScaleByValue(SetHandleValue(MINScale, MAXScale, hipHandle, hips), hipBones);
+                        break;
+                    case "waist":
+                        waist = waistHandle.AssignText();
+                        ScaleByValue(SetHandleValue(MINScale, MAXScale, waistHandle, waist), waistBones);
+                        break;
+                    case "shoulder":
+                        shoulder = shoulderHandle.AssignText();
+                        ScaleByValue(SetHandleValue(MINScale, MAXScale, shoulderHandle, shoulder), shoulderBones);
+                        break;
+                    case "neck":
+                        neck = neckHandle.AssignText();
+                        ScaleByValue(SetHandleValue(MINScale, MAXScale, neckHandle, neck), neckBones);
+                        break;
+                    default:
+                        Debug.Log("No available handle to scale");
+                        break;
+                }
+            }
             
         }
-
         
-
-        private void ScaleChestByValue(float scaleValue)
+        private static void ScaleByValue(float scaleValue, IEnumerable<GameObject> bones)
         {
-            foreach (var bone in chestObject)
+            foreach (var bone in bones)
             {
                 Debug.Log(bone.GetComponent<Transform>().localScale = new Vector3(scaleValue, scaleValue, scaleValue));
             }
         }
-        
     }
 }
