0574a2f96e8295ae054465494d7fbb816324924c
Laura <lauramarie8131@gmail.com>
2021-02-26 21:55:22 +0100

    Json files read in - BUG

---
+++ VirtualOnlineShopping/Assets/Scripts/Clothing/ClothingObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Clothing
+{
+    public class ClothingObject : MonoBehaviour
+    {
+        private int id; 
+        private string name;
+        private string colour;
+        private string brandName;
+        private int productCode;
+        private string url;
+        private string imageUrl; 
+        
+    
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
