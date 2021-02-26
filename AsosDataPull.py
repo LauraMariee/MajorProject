d6d2cbb46d024ff926abbd3a4747d00d3b16f7d4
Laura <lauramarie8131@gmail.com>
2021-02-26 16:07:01 +0100

    Refined script

--- AsosDataPull.py
+++ AsosDataPull.py
@@ -1,32 +1,34 @@
 import requests
-import json
 
-url = "https://asos2.p.rapidapi.com/products/v2/list"
-
-querystring = {"offset":"0","categoryId":"4209","limit":"48","store":"US","country":"US","currency":"USD","sort":"","lang":"en-US","sizeSchema":"US"}  # shoes, boots and sneakers
-
-
-# womens catagories
+# womens categories
 # 11321 - hoodies and sweatshirts
 # 4169 - Tops
 # 3630 - Jeans
 # 8799 - Dress's
 
-# men catagories
+# men categories
 # 7616 - Tshirts and vests
 # 4208 - Jeans
 # 5668 - Hoodies
 # 18797 - loungewear
 
-headers = {
-    'x-rapidapi-key': "4e18bf3b86mshdd530c160a389acp1dec3ejsn2a0de404f414",
-    'x-rapidapi-host': "asos2.p.rapidapi.com"
+
+url = "https://asos2.p.rapidapi.com/products/v2/list"
+
+clothingCategory = ["11321", "4169", "3630", "8799", "7616", "4208", "5668", "18797"]
+for x in clothingCategory:
+    querystring = {"offset": "0", "categoryId": x, "limit": "48", "store": "US", "country": "US",
+                   "currency": "USD", "sort": "", "lang": "en-US", "sizeSchema": "US"}  # shoes, boots and sneakers
+
+    headers = {
+        'x-rapidapi-key': "4e18bf3b86mshdd530c160a389acp1dec3ejsn2a0de404f414",
+        'x-rapidapi-host': "asos2.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-writeFile = open('testFile.json', 'w')
-writeFile.write(response.text)
-writeFile.close()
+    writeFile = open( x + '.json', 'w')
+    writeFile.write(response.text)
+    writeFile.close()
 
-print(response.text)
+    print(response.text)
