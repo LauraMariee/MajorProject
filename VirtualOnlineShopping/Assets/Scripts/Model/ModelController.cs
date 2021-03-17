435d6bf095e37d201c792c44609d4c2bfe747c84
Laura <lauramarie8131@gmail.com>
2021-03-17 12:47:51 +0100

    Models change position

--- VirtualOnlineShopping/Assets/Scripts/Model/ModelController.cs
+++ VirtualOnlineShopping/Assets/Scripts/Model/ModelController.cs
@@ -26,14 +26,12 @@ namespace Model
             foreach (var button in buttons)
             {
                 var switchButton = button.GetComponent<SwitchModel>();
-                if (switchButton.HasButtonBeenPressed())
-                {
-                    var oldActive = activeModel;
-                    activeModel = switchButton.GETModel();
-                    switchButton.ResetButton();
-                    // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-                    MoveModel(oldActive, activeModel);
-                }
+                if (!switchButton.HasButtonBeenPressed()) continue;
+                var oldActive = activeModel;
+                activeModel = switchButton.GETModel();
+                switchButton.ResetButton();
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                MoveModel(oldActive, activeModel);
             }
         }
 
@@ -46,25 +44,13 @@ namespace Model
             }
 
             // move active model into spotlight
-            if (newActive != null)
-            {
-                // remember where it came from so that we can move it back there
-                activeModelOrigin = newActive.GetComponent<Transform>().position;
+            if (newActive == null) return;
+            // remember where it came from so that we can move it back there
+            activeModelOrigin = newActive.GetComponent<Transform>().position;
                 
-                // WE WANT TO MOVE IT, MOVE IT! MOVE IT!
-                if (newActive.GetComponent<FemaleModel>())
-                {
-                    newActive.GetComponent<Transform>().localPosition = spawnPosition; //Replace with vector3
-                }
-                else if (newActive.GetComponent<MaleModel>())
-                {
-                    newActive.GetComponent<Transform>().localPosition = spawnPosition; //Replace with vector3
-                    //newActive.GetComponent<Transform>().position = new Vector3(-10.89f, 4.39f, 7.71f); //Replace with vector3
-                }
-            }
-            //DisplayCurrentModel();
-            
-            
+            // WE WANT TO MOVE IT, MOVE IT! MOVE IT!
+            newActive.GetComponent<Transform>().position = spawnPosition; //Replace with vector3
+            Debug.Log(spawnPosition + "Female");
         }
     }
 }
