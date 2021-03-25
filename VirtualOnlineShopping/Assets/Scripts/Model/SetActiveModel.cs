9c8906ba2fc02723e0d1e21b19ba34bc2747e72c
Laura <lauramarie8131@gmail.com>
2021-03-25 16:11:28 +0100

    Started box collider to deactivate slider

---
+++ VirtualOnlineShopping/Assets/Scripts/Model/SetActiveModel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Model
+{
+    public class SetActiveModel : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var modelObject = other.gameObject;
+            Debug.Log(modelObject + "Enter");
+            
+            if (!modelObject.tag.Equals("Model")) return;
+        
+            if(modelObject.GetComponentInParent(typeof(MaleModel)) as MaleModel)
+            {
+                Debug.Log("Male is enabled");
+                modelObject.GetComponentInParent<MaleModel>().enabled = true; 
+            }
+            if(modelObject.GetComponentInParent(typeof(FemaleModel)) as FemaleModel)
+            {
+                Debug.Log("Female is enabled");
+                modelObject.GetComponentInParent<FemaleModel>().enabled = false; 
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log(other + "Exit");
+            
+            if (!other.gameObject.tag.Equals("Model")) return;
+        
+            if(other.gameObject.GetComponentInParent(typeof(MaleModel)) as MaleModel)
+            {
+                Debug.Log("Male is disabled");
+                other.gameObject.GetComponentInParent<MaleModel>().enabled = false; 
+            }
+            if(other.gameObject.GetComponentInParent(typeof(FemaleModel)) as FemaleModel)
+            {
+                Debug.Log("Female is disabled");
+                other.gameObject.GetComponentInParent<FemaleModel>().enabled = false; 
+            }
+        }
+    }
+}
