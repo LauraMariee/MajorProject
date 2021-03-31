ca8146212094420190837c9e93bdaf2901057345
Laura <lauramarie8131@gmail.com>
2021-03-31 16:55:46 +0200

    Diss and diagram work

---
+++ VirtualOnlineShopping/Assets/Scripts/Filtering/FilterButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Filtering
+{
+    public class FilterButton : MonoBehaviour
+    {
+        public Button button;
+        private bool clicked; 
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        public void OnClick()
+        {
+            clicked = true; 
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
