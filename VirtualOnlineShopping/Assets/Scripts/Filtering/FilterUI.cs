88c4b45bc0bd7c9e256bcaf1ba375101664eaeae
Laura <lauramarie8131@gmail.com>
2021-04-02 12:17:20 +0200

    item filter colours

--- VirtualOnlineShopping/Assets/Scripts/Filtering/FilterUI.cs
+++ VirtualOnlineShopping/Assets/Scripts/Filtering/FilterUI.cs
@@ -7,13 +7,38 @@ namespace Filtering
     {
         private string colour;
         private string brandName;
-        
+
+
         // Start is called before the first frame update
         void Start()
         {
         
         }
 
+        public void ColourCLick()
+        {
+            DetailMenuCheck(); //Check if detail menu open 
+            //replace buttons if yes
+            //Show Detail menu if no
+            //save singular option 
+        }
+
+        private static bool DetailMenuCheck()
+        {
+            var detailPanel = GameObject.Find("UI/Canvas/FilterUI/detailsMenu");
+            if(detailPanel)
+            //find detail model check
+            //if detail model is active return true
+            //if detail model is not active then return false
+        }
+        
+        public void BrandCLick()
+        {
+            //Check if detail menu open 
+            //replace buttons if yes
+            //Show Detail menu if no
+        }
+        
         private void Save()
         {
         
